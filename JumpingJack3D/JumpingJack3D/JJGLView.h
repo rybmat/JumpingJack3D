0ebf0c28a6a7451dd2df00e70bca45a81b079a02
Mateusz Rybarski <mateusz@MacBook-Pro-Mateusz.local>
2013-07-10 21:02:44 +0200

    added NSOpenGLViev subclass with basic rendering stuff

---
+++ JumpingJack3D/JumpingJack3D/JJGLView.h
@@ -0,0 +1,20 @@
+//
+//  JJGLView.h
+//  JumpingJack3D
+//
+//  Created by Mateusz Rybarski on 10.07.2013.
+//  Copyright (c) 2013 Mateusz Rybarski. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <Foundation/Foundation.h>
+#import <OpenGL/gl3.h>
+
+#import "glm/glm.hpp"
+#import "glm/gtc/matrix_transform.hpp"
+#import "glm/gtc/type_ptr.hpp"
+
+@interface JJGLView : NSOpenGLView
+
+@end
---
+++ JumpingJack3D/JumpingJack3D/JJGLView.mm
@@ -0,0 +1,149 @@
+//
+//  JJGLView.mm
+//  JumpingJack3D
+//
+//  Created by Mateusz Rybarski on 10.07.2013.
+//  Copyright (c) 2013 Mateusz Rybarski. All rights reserved.
+//
+
+#import "JJGLView.h"
+
+@implementation JJGLView
+
+NSTimer *renderTimer;
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void) prepareOpenGL{
+    [super prepareOpenGL];
+    [self initOpenGL];
+}
+
+-(void) initOpenGL {
+	
+    
+    
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_DEPTH_TEST);
+}
+
+- (void) dealloc{
+    
+}
+
+- (void) awakeFromNib{
+    
+    NSOpenGLPixelFormatAttribute attrs[] =
+	{
+		NSOpenGLPFADoubleBuffer,
+		NSOpenGLPFADepthSize, 24,
+        
+		NSOpenGLPFAOpenGLProfile,
+		NSOpenGLProfileVersion3_2Core,
+        
+		0
+	};
+	
+	NSOpenGLPixelFormat *pf = [[NSOpenGLPixelFormat alloc] initWithAttributes:attrs];
+	
+	if (!pf)
+	{
+		NSLog(@"No OpenGL pixel format");
+	}
+    
+    NSOpenGLContext* context = [[NSOpenGLContext alloc] initWithFormat:pf shareContext:nil];
+    
+    [self setPixelFormat:pf];
+    
+    [self setOpenGLContext:context];
+    
+    
+    renderTimer = [NSTimer timerWithTimeInterval:0.001
+                                          target:self
+                                        selector:@selector(timerFired:)
+                                        userInfo:nil
+                                         repeats:YES];
+    [[NSRunLoop currentRunLoop] addTimer:renderTimer
+                                 forMode:NSDefaultRunLoopMode];
+    
+    [[NSRunLoop currentRunLoop]
+     addTimer:renderTimer
+     forMode:NSEventTrackingRunLoopMode];
+}
+
+- (void) timerFired: (id)sender{
+    
+    [self nextFrame];
+    [self display];
+}
+
+- (void) nextFrame{
+    
+    //calculateing new positions here
+    
+    // np nowe pozycje poruszających sie klocków
+    
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    glClearColor(0,0,0,1);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    // Drawing code here.
+    
+    glFlush();
+}
+
+- (BOOL)canBecomeKeyView {
+    return  YES;
+}
+- (BOOL)acceptsFirstResponder {
+    return  YES;
+}
+
+- (void) keyDown:(NSEvent *)theEvent{
+    
+    NSString *theArrow = [theEvent charactersIgnoringModifiers];
+    unichar keyChar = 0;
+    if ( [theArrow length] == 0 )
+        return;
+    if ( [theArrow length] == 1 ) {
+        keyChar = [theArrow characterAtIndex:0];
+        if ( keyChar == NSLeftArrowFunctionKey ) {
+            
+            return;
+        }
+        if ( keyChar == NSRightArrowFunctionKey ) {
+         
+            return;
+        }
+        if ( keyChar == NSUpArrowFunctionKey ) {
+           
+            return;
+        }
+        if ( keyChar == NSDownArrowFunctionKey ) {
+            
+            return;
+        }
+        if ( keyChar == ' '){
+            
+        }
+        
+        
+        //[super keyDown:theEvent];
+    }
+    //[super keyDown:theEvent];
+    
+}
+
+@end
