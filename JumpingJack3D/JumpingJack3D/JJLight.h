06cac73ce93c6690824c19ac8f84a04839be1fe4
Maciej <maciej.zurad@gmail.com>
2013-07-16 03:56:55 +0200

    Adding JJLight class

---
+++ JumpingJack3D/JumpingJack3D/JJLight.h
@@ -0,0 +1,13 @@
+//
+//  JJLight.h
+//  JumpingJack3D
+//
+//  Created by Maciej Żurad on 7/16/13.
+//  Copyright (c) 2013 Mateusz Rybarski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JJLight : NSObject
+
+@end
